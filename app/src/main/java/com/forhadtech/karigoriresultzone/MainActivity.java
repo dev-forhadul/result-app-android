3598063b72f1af6394aa78a1d8ad32e5fe451668
Forhadul Islam <forhadul75@gmail.com>
2023-11-03 10:35:26 +0600

    first commit

---
+++ app/src/main/java/com/forhadtech/karigoriresultzone/MainActivity.java
@@ -0,0 +1,14 @@
+package com.forhadtech.karigoriresultzone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
